f23511edbac4058b02529f157c8ed1e3820f3865
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-05-18 15:44:49 +0500

    ES6 string addtion

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function joinStrings(string1, string2){
-   return `${string1} ${string2}`
-}
-console.log( joinStrings());
+// exercies : "ES6 string addtion"
+
+// sharti : Ikki qatorni shu tarzda birlashtirish oson string1 + string2.
+// Istalgan natijaga erishishning yana bir usuli - bustring1.concat(string2)
+// ES6 satrlarni birlashtirishning yana bir usulini taqdim etdi. Sizning vazifangiz bu nima ekanligini aniqlash va ikkita satrni bir-biriga qo'shadigan funktsiyani yozish, ikkita satr orasida bo'sh joy bo'lishi kerak.
+
+// +, .concat()& .join()bu mashqda ruxsat berilmaydi. Men aldash uchun ishlatilishi mumkin bo'lgan boshqa usullarni ham olib tashladim!
+// Agar argumentlardan biri raqam bo'lsa, kodingiz uni satrga majburlashi kerak.
+
+//===========================================================================
+
+// function joinStrings(string1, string2){
+//    return `${string1} ${string2}`
+// }
+// console.log( joinStrings());
+
+// masala linki : https://www.codewars.com/kata/582e4c3406e37fcc770001ad/train/javascript  //8kyu
+//===========================================================================
+
+
+
